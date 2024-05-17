41fd11a971d557a7b7c590cf87815afe0973415c
jerinpious <jpious4925@conestogac.on.ca>
2024-05-17 15:41:15 -0400

    model and base

---
+++ portfolio/main/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique= True)
+
+    def __str__(self):
+        return self.name
+       
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name = "projects")
+    # so the tags will basically allow each of the prijects to have a tag like javascript, python, css and we will be able identify the projects basedc on the tags
+    link = models.URLField(max_length= 200, blank=True )
+
+    def __str__(self):
+        return self.title
+    
+class ProjectImage(models.Model):
+    project = models.ForeignKey(
+        Project, related_name="images", on_delete=models.CASCADE
+        )
+    image = models.ImageField(upload_to="project_images/")
+
+    def __str__(self):
+        return f"{self.project.title} Image"
+    
+
