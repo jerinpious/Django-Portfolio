64e6a64c13fd5d51427c287713db0bab17a1236c
jerinpious <jpious4925@conestogac.on.ca>
2024-05-18 19:57:59 -0400

    complete project

--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -4,10 +4,17 @@ from .models import Project,Tag
 # Create your views here.
 
 def home(request):
-    return render(request,"home.html")
+    projects = Project.objects.all() # this will fetch all the projects that we created this can be done because of the Object Relational Mapping of Django
+    tags = Tag.objects.all()
+    return render(request,"home.html",{"projects":projects, "tags":tags}) # we willl pass a dictionary which will have key value pairs for all the projects and tags to render the dynamic data
+    # now what will happen is that in our home.html we will be able to access the variable python which contains all our prijects and variable tags with all the tag
+    
 
 def contact(request):
     return render(request,"contact.html")
 
 def project(request,id):
-    return render(request, "project.html")
+    project = get_object_or_404(Project, pk=id) # the get_object_404 is a helper function which will look for teh model which have the key
+    #in this case we are looking for the primary key which will be the id we have specified in the Project model and if it exists return it and of not will return a 404 error page\
+
+    return render(request, "project.html", {"project":project}) # passing a project dictionary to the template
